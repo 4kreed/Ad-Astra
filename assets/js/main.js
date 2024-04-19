e36f4f9cc8d75c0156ea2719c57887c5dc3a902a
4kreed <4kr33d@gmail.com>
2024-04-19 16:51:44 +0000

    Added mode toggle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
-const toggleSearch = (search, button) => {
-    const searchBar = document.getElementById(search),
-            searchButton = document.getElementById(button);
+function toggleSearch(searchId, buttonId) {
+    const searchBar = document.getElementById(searchId);
+    const searchButton = document.getElementById(buttonId);
 
     searchButton.addEventListener('click', () => {
         searchBar.classList.toggle('show-search');
-    })
+    });
+}
+
+function toggleMode() {
+    var body = document.body;
+    var logo = document.getElementById('logo');
+
+    body.classList.toggle('light-mode');
+    body.classList.toggle('dark-mode');
+
+    logo.src = body.classList.contains('dark-mode') ? "/assets/img/white-logo.webp" : "/assets/img/black-logo.webp";
 }
 
 toggleSearch('search-bar', 'search-button')
+const toggle = document.getElementById('toggle');
+toggle.addEventListener('change', toggleMode);
