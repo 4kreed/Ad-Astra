126b7fa09e1e91722179a89d33bdfa850ebdcb8b
4kreed <4kr33d@gmail.com>
2024-05-03 16:59:50 +0000

    Improved web modes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,47 @@ function toggleSearch(searchId, buttonId) {
     });
 }
 
-function toggleMode() {
-    var body = document.body;
-    var logo = document.getElementById('logo');
+function setMode(mode) {
+    localStorage.setItem("theme", mode);
+  
+    document.body.classList.remove('dark-mode', 'light-mode');
+  
+    document.body.classList.add(mode);
+  
+    const logo = document.getElementById('logo');
+    if (mode === 'dark-mode') {
+      logo.src = "/assets/img/white-logo.webp";
+    } else {
+      logo.src = "/assets/img/black-logo.webp";
+    }
 
-    body.classList.toggle('light-mode');
-    body.classList.toggle('dark-mode');
-
-    logo.src = body.classList.contains('dark-mode') ? "/assets/img/white-logo.webp" : "/assets/img/black-logo.webp";
+    const toggle = document.getElementById('toggle');
+    toggle.checked = mode === 'light-mode';
 }
 
-toggleSearch('search-bar', 'search-button')
+function toggleMode() {
+    const currentMode = document.body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode';
+    const newMode = currentMode === 'dark-mode' ? 'light-mode' : 'dark-mode';
+    setMode(newMode);
+}
+
+function setInitialMode() {
+    // Get the user's theme preference from local storage, if it's available
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+        setMode(savedTheme);
+    } else {
+        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        if (prefersDarkMode) {
+            setMode("dark-mode");
+        } else {
+            setMode("light-mode");
+        }
+    }
+}
+
+toggleSearch('search-bar', 'search-button');
 const toggle = document.getElementById('toggle');
 toggle.addEventListener('change', toggleMode);
+
+setInitialMode();
