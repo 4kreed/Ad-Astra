88f1859eb8e51e827425aeaec51f2ea790debd11
4kreed <4kr33d@gmail.com>
2024-06-03 11:48:29 +0000

    Migrate to Astro. Landing page done.

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Ad Astra";
+export const SITE_DESCRIPTION = "Welcome to my website!";
---
+++ tailwind.config.mjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            fontFamily: {
+                header: ["Adam CG Pro", "sans-serif"],
+                404: ["Nova Square", "sans-serif"],
+                chakra: ["Chakra Petch", "sans-serif"],
+            },
+            transitionTimingFunction: {
+                "custom-bezier": "cubic-bezier(0.9, 0, 0.3, 0.9)",
+            },
+            keyframes: {
+                floatAnimation: {
+                    "0%": { transform: "scaleX(-1) translateY(0)" },
+                    "50%": { transform: "scaleX(-1) translateY(-20px)" },
+                    "100%": { transform: "scaleX(-1) translateY(0)" },
+                },
+                moveStars: {
+                    from: { transform: "translateY(0)" },
+                    to: { transform: "translateY(-100vh) " },
+                },
+            },
+            animation: {
+                float: "floatAnimation 4s ease-in-out infinite",
+                moveStars: "moveStars var(--duration) linear infinite",
+            },
+            boxShadow: {
+                "space-layer": "var(--space-layer)",
+            },
+        },
+    },
+    plugins: [],
+};
