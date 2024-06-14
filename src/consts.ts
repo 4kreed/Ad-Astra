794a3fba5275e980c175dd0a2304a733df9288c9
4kreed <4kr33d@gmail.com>
2024-06-14 16:19:41 +0200

    About page.

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+
+export const contributors = [
+    // keeping this as an example
+    // {
+    //     name: "Alvaro Lopez (@Akreed)",
+    //     logo: "https://avatars.githubusercontent.com/u/22918075",
+    //     href: "https://github.com/4kreed",
+    // },
+    {
+        name: "Asier Luque",
+    },
+];
